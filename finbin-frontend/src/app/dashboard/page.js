a26e872bf99c1097bf0cca77d70c160f6c624ec7
DoubleU <yuuliu.5533@gmail.com>
2025-03-22 03:15:59 -0400

    Finished Transactions page

--- finbin-frontend/src/app/dashboard/page.js
+++ finbin-frontend/src/app/dashboard/page.js
@@ -14,13 +14,13 @@ import { Button } from "@/components/ui/button"
 
 export default function Dashboard(){
     return(
-        <div className="flex flex-col w-full h-screen gap-4">
+        <div className="flex flex-col w-full h-screen gap-10">
             <ThemeToggle />
             <div className="w-full text-3xl">
                 Dashboard
             </div>
 
-            <div className="grid grid-cols-4 gap-4">
+            <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 <div>
                     <Card>
                         <CardContent>
@@ -59,7 +59,7 @@ export default function Dashboard(){
                 </div>
             </div>
 
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                 <div>
                     <Card>
                         <CardHeader>
@@ -104,7 +104,7 @@ export default function Dashboard(){
                 </div>
             </div>
 
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
                     <Card>
                         <CardHeader>
--- finbin-frontend/src/app/transactions/page.js
+++ finbin-frontend/src/app/transactions/page.js
@@ -7,13 +7,84 @@ import {
     CardTitle,
   } from "@/components/ui/card"
 
+  import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+
+  import { Input } from "@/components/ui/input"
+
+  import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
 import { Button } from "@/components/ui/button"
+import { ThemeToggle } from "@/components/theme-toggle"
 
 
 export default function Transaction(){
+
+
+    const invoices = [
+        {
+          invoice: "INV001",
+          paymentStatus: "Paid",
+          totalAmount: "$250.00",
+          paymentMethod: "Credit Card",
+        },
+        {
+          invoice: "INV002",
+          paymentStatus: "Pending",
+          totalAmount: "$150.00",
+          paymentMethod: "PayPal",
+        },
+        {
+          invoice: "INV003",
+          paymentStatus: "Unpaid",
+          totalAmount: "$350.00",
+          paymentMethod: "Bank Transfer",
+        },
+        {
+          invoice: "INV004",
+          paymentStatus: "Paid",
+          totalAmount: "$450.00",
+          paymentMethod: "Credit Card",
+        },
+        {
+          invoice: "INV005",
+          paymentStatus: "Paid",
+          totalAmount: "$550.00",
+          paymentMethod: "PayPal",
+        },
+        {
+          invoice: "INV006",
+          paymentStatus: "Pending",
+          totalAmount: "$200.00",
+          paymentMethod: "Bank Transfer",
+        },
+        {
+          invoice: "INV007",
+          paymentStatus: "Unpaid",
+          totalAmount: "$300.00",
+          paymentMethod: "Credit Card",
+        },
+        
+      ]
+
+
     return(
-        <div className="flex flex-col w-full h-screen text-black gap-4">
+        <div className="flex flex-col w-full h-screen gap-5 p-6 min-w-[700px]">
             <ThemeToggle />
+            <h1 className="text-2xl font-bold">Transactions</h1>
             <div className = "grid grid-cols-3 gap-4">
                 <div>
                     <Card>
@@ -43,9 +114,82 @@ export default function Transaction(){
                     </Card>
                 </div>
             </div>
+{/* TABLE */}
             <div>
-                
+                <Card>
+                    <CardContent className="flex flex-col gap-4">
+                        <CardTitle>Transaction History</CardTitle>
+                        <CardDescription>View and filter your transaction history</CardDescription>
+
+                        <div className="flex justify-between items-center mb-4 gap-4">
+                            <Input placeholder="Search by invoice number" className="w-full" />
+                            <div className="flex flex-row gap-1">
+
+                                <Select>
+                                    <SelectTrigger className="w-[180px]">
+                                        <SelectValue placeholder="Category" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="All Category">All Category</SelectItem>
+                                        <SelectItem value="Groceries">Groceries</SelectItem>
+                                        <SelectItem value="Utilities">Utilities</SelectItem>
+                                    </SelectContent>
+                                </Select>
+
+                                <Select>
+                                    <SelectTrigger className="w-[180px]">
+                                        <SelectValue placeholder="Time Period" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="Today">Today</SelectItem>
+                                        <SelectItem value="Last 7 Days">Last 7 Days</SelectItem>
+                                        <SelectItem value="Last 30 days">Last 30 days</SelectItem>
+                                        <SelectItem value="Last Year">Last Year</SelectItem>
+                                    </SelectContent>
+                                </Select>
+
+                                <Button className="w-[180px]">
+                                    Export
+                                </Button>
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between items-center mb-4">
+                            <div>
+                                <CardTitle>Recent Transactions</CardTitle>
+                                <CardDescription>Your latest financial activities </CardDescription>
+                            </div>   
+                            <Button className="mb-4">Add Transaction</Button>
+                        </div>
+
+                        <Table>
+                            <TableCaption>A list of your recent invoices.</TableCaption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead className="w-[100px]">Invoice</TableHead>
+                                    <TableHead>Status</TableHead>
+                                    <TableHead>Method</TableHead>
+                                    <TableHead className="text-right">Amount</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+
+                                {invoices.map((invoice) => (
+                                    <TableRow key={invoice.invoice}>
+                                        <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                                        <TableCell>{invoice.paymentStatus}</TableCell>
+                                        <TableCell>{invoice.paymentMethod}</TableCell>
+                                        <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                                    </TableRow>
+                                ))}
+
+                            </TableBody>
+                        </Table>
+
+                    </CardContent>
+                </Card>
             </div>
+{/* TABLE */}
         </div>
     )
 }
